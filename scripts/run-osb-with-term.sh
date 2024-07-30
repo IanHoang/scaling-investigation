2be9fc6544c4300c065613a497cc8d025bd5b6ec
Ian Hoang <hoangia@amazon.com>
2024-07-30 14:08:28 -0500

    Update run-osb-with-term.sh script
    Signed-off-by: Ian Hoang <hoangia@amazon.com>

--- scripts/run-osb-with-term.sh
+++ scripts/run-osb-with-term.sh
@@ -9,4 +9,4 @@ new_uuid=${uuid// }
 new_run_id="${run_id}-${new_hostname}-${new_uuid}"
 
 # Fill out basic_auth_user and basic_auth_password first
-opensearch-benchmark execute-test --workload=big5 --pipeline=benchmark-only --target-hosts=$host --client-options="basic_auth_user:'',basic_auth_password:''" --workload-params=test_iterations:10000,search_clients:1,target_throughput:"" --include-tasks=term --kill-running-processes --results-file=/home/ec2-user/$new_run_id --test-execution-id=$new_run_id
+opensearch-benchmark execute-test --workload=big5 --pipeline=benchmark-only --target-hosts=$host --client-options="basic_auth_user:'',basic_auth_password:''" --workload-params=time_period:600,search_clients:1,target_throughput:"" --include-tasks=term --kill-running-processes --results-file=/home/ec2-user/$new_run_id --test-execution-id=$new_run_id
