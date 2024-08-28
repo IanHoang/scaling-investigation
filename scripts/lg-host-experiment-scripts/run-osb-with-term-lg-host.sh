81b71dc5f5d4a8200e4564c8bfe5f398ac25de8d
Hoang <hoangia@80a9971b1103.amazon.com>
2024-08-28 13:43:31 -0500

    Update script to print out the command to console since there are issues with running the target_throughput: in OSB
    Signed-off-by: Hoang <hoangia@80a9971b1103.amazon.com>

--- scripts/lg-host-experiment-scripts/run-osb-with-term-lg-host.sh
+++ scripts/lg-host-experiment-scripts/run-osb-with-term-lg-host.sh
@@ -9,4 +9,4 @@ new_run_id="${run_id}-${new_hostname}"
 params="time_period:600,search_clients:${clients},target_throughput:\"\""
 
 # Fill out basic_auth_user and basic_auth_password first
-opensearch-benchmark execute-test --workload=big5 --pipeline=benchmark-only --target-hosts=$host --client-options="basic_auth_user:'hoangia',basic_auth_password:'Hoangia@123'" --workload-params=$params --include-tasks=term --kill-running-processes --results-file=/home/ec2-user/$new_run_id --test-execution-id=$new_run_id
+echo opensearch-benchmark execute-test --workload=big5 --pipeline=benchmark-only --target-hosts=$host --client-options="basic_auth_user:'hoangia',basic_auth_password:'Hoangia@123'" --workload-params=$params --include-tasks=term --kill-running-processes --results-file=/home/ec2-user/$new_run_id --test-execution-id=$new_run_id
